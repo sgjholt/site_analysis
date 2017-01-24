2ecb600c1132d0f003100577f2963750329a91d7
james <jamesholt92@hotmail.com>
2017-01-24 14:24:48 +0000

    added forward model method and misfit method to Sim1D class

--- sim1D.py
+++ sim1D.py
@@ -6,29 +6,71 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-class Sim1D(sc.Site, sm.Site1D, freqs=None):
-    def __init__(self, name, working_directory, freqs, vel_file_dir=None):
+class Sim1D(sc.Site, sm.Site1D):
+    def __init__(self, name, working_directory, vel_file_dir=None):
+
         sc.Site.__init__(self, name, working_directory, vel_file_dir)
         sm.Site1D.__init__(self)
         self.__add_site_profile()
+
+    def __add_site_profile(self):
+        if not self.has_vel_profile:
+            return None
+
+        vels = self.get_velocity_profile()
+        for i, hl in enumerate(vels['thickness']):
+            self.AddLayer([hl, vels['vp'][i], vels['vs'][i], vels['rho'][i], 1, 1])
+
+    def elastic_forward_model(self, i_ang=0, elastic=True, plot_on=False, show=False, freqs=None):
+
+        if not self.has_vel_profile:
+            print('Cannot model - no velocity model')
+            return None
+
         if freqs is None:
             self.GenFreqAxis(0.1, 25, 1000)
         else:
             self.GenFreqAxis(freqs[0], freqs[1], freqs[3])
 
-    def __add_site_profile(self):
-        vels = self.get_velocity_profile()
-        for i, hl in enumerate(vels['thickness']):
-            self.AddLayer([hl, vels['vp'][i], vels['vs'][i], vels['rho'][i], 1, 1])
-
-    def forward_model(self, i_ang=0, elastic=True, plot_on=False, show=False):
-
         shtf = self.ComputeSHTF(i_ang, elastic)
 
         if plot_on:
+            plt.title('{0} : 1D Linear Elastic SHTF'.format(self.site))
             plt.loglog(self.Freqs, np.abs(shtf), label='SHTF')
+            plt.hlines(1, 0.1, 25, linestyles='dashed', colors='red')
+            plt.xlabel('Frequency [Hz]')
+            plt.ylabel('SHTF')
 
         else:
             return np.abs(shtf)
+        if show:
+            plt.show()
+
+    def misfit(self, i_ang=0, elastic=True, plot_on=False, show=False):
+
+        observed = self.sb_ratio()  # pandas DataFrame object
+
+        freqs = (round(float(
+            observed.columns.values[0]), 2), round(float(
+            observed.columns.values[-1]), 2), observed.columns.values.__len__())
+
+        predicted = self.elastic_forward_model(i_ang, elastic, freqs=freqs)
+
+        if predicted is None:  # No forward model - return nothing
+            print('Misfit not available - no forward model.')
+            return None  # return nothing to break out of function
+
+        log_residuals = np.log(predicted) - np.log(observed)
+
+        log_rms_misfit = (np.sum(log_residuals ** 2) / len(log_residuals)) ** 0.5
+
+        if plot_on:
+            plt.title('{0} : Log Residuals - Log RMS Misfit: {1}.'.format(self.site, round(log_rms_misfit), 2))
+            plt.loglog(self.Freqs, log_residuals, 'ko', label='Residuals')
+            plt.hlines(0, 0.1, 25, linestyles='dashed', colors='red')
+            plt.xlabel('Frequency [Hz]')
+            plt.ylabel('Log Residuals')
+        else:
+            return log_residuals, log_rms_misfit
         if show:
             plt.show()
