cc25f0b9258d6a754ae8c465424ddda18e3e8872
james <jamesholt92@hotmail.com>
2017-02-22 12:24:00 +0100

    Updated simulation code, added new titles to dataframe object to store results

--- random_search.py
+++ random_search.py
@@ -19,21 +19,20 @@ def main():
     iterations = 1000
     name = site+'_rnd_ufm_'+'_'+str(iterations)+'_run_0'
     wd = '/data/share/Japan/SiteInfo/S_B/Repi_lessthan_300_depth_lessthan_25/'
-    rd = '/data/share/Japan/SiteInfo/S_B/{0}_Vs_MC'.format(site)
+    rd = '/data/share/Japan/SiteInfo/S_B/{0}_Vs_MC/'.format(site)
 
     if not os.path.isdir(rd):  # if not a directory, make it
         print('creating dir: {0}'.format(rd))
         os.mkdir(rd)
 
-    if os.path.exists(rd+'/'+name+'.csv'):
+    if os.path.exists(rd+name+'.csv'):
         j = 0
-        while os.path.exists(name + '.csv'):
+        while os.path.exists(rd+name+'.csv'):
             j += 1  # if the file already exists, give it another name
             name = name.replace('run_0', 'run_{0}'.format(j))
         print('name changed to: {0}'.format(name))
 
     uniform_search(site, wd, rd, 25, 100, iterations, name, (0.4, 0.6), 1, 0, (0, 25), False, False)
-
 
 
 def uniform_search(*args):
@@ -42,7 +41,7 @@ def uniform_search(*args):
              'i_ang', 'x_range_for_xcor', 'elastic', 'cadet_correct']
     vrs = [site, wd, rd, pct, steps, iters, weights, lam, i_ang, x_range, elastic, cadet_correct]
 
-    with open(rd+'/'+name+'.cfg', 'wt') as f:
+    with open(rd+name+'.cfg', 'wt') as f:
         f.write('config file for {0}'.format(name).capitalize())
         f.write('\n')
         f.write('\n')
