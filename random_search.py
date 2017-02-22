5c20375cc8bf5cf883490ab449acdfd882858722
james <jamesholt92@hotmail.com>
2017-02-22 11:00:04 +0100

    Updated simulation code, added new titles to dataframe object to store results

--- random_search.py
+++ random_search.py
@@ -1,6 +1,6 @@
 __author__ = 'James Holt'
 
-# site_class - Base Site class object - handles site metadata - produces plots and other useful helper functions.
+# random_search - script to run random velocity profile searches
 # VERSION: 0.9
 # AUTHOR(S): JAMES HOLT - UNIVERSITY OF LIVERPOOL
 #
@@ -16,24 +16,34 @@ import os
 
 def main():
     site = 'FKSH16'
-    iterations=1000
-    name = 'random_uniform_'+site+str(iterations)
+    iterations = 1000
+    name = 'random_uniform_'+site+'_'+str(iterations)
     wd = '/data/share/Japan/SiteInfo/S_B/Repi_lessthan_300_depth_lessthan_25/'
     rd = '/data/share/Japan/SiteInfo/S_B/{0}_Vs_MC'.format(site)
     if not os.path.isdir(rd):
         os.mkdir(rd)
-    if os.path.isfile(rd+'/'+name):
-        name.join('1')
-    uniform_search(site, 25, 100, iterations, name, (0.4, 0.6), 1, 0, (0, 25), True, False)
+    uniform_search(site, wd, rd, 25, 100, iterations, name, (0.4, 0.6), 1, 0, (0, 25), False, False)
 
 
 def uniform_search(*args):
-    site, wd, rd, pct, steps, iters, name, weights, lam, i_ang, x_range, elastic, cadet_correct  = args
+    site, wd, rd, pct, steps, iters, name, weights, lam, i_ang, x_range, elastic, cadet_correct = args
+    names = ['site', 'SB_dir', 'run_dir', 'pct_variation', 'model_steps', 'iterations', 'misfit_weights', 'lambda',
+             'i_ang', 'x_range_for_xcor', 'elastic', 'cadet_correct']
+    vrs = [site, wd, rd, pct, steps, iters, weights, lam, i_ang, x_range, elastic, cadet_correct]
+
+    with open(rd+'/'+name+'.cfg', 'wt') as f:
+        f.write('config file for {0}'.format(name).capitalize())
+        f.write('\n')
+        f.write('\n')
+        for i, name in enumerate(names):
+            f.write("{0} = {1}".format(name, vrs[i]))
+            f.write('\n')
 
     site = sd.Sim1D(site, working_directory=wd, run_dir=rd)
 
     site.uniform_random_search(pct_variation=pct, steps=steps, iterations=iters, name=name, weights=weights, lam=lam,
-                               i_ang=i_ang, x_cor_range=x_range, elastic=elastic, cadet_correct=cadet_correct, save=True)
+                               i_ang=i_ang, x_cor_range=x_range, elastic=elastic, cadet_correct=cadet_correct,
+                               save=True)
 
 if __name__ == '__main__':
     start_time = time.time()
