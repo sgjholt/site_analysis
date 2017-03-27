fixed misfit method in Sim1D class - misfit calculated incorrectly resulting in wrong shape output matrix. Now subtraction is calculated correctly through subtraction of vectors with identical shape,

sim1D.py
@@ -230,7 +230,7 @@ class Sim1D(sc.Site, sm.Site1D):
             # must change Thicknesses, Density, Vp, Vs, Qp an Qs
             if len(self.Mod['Hl']) != (len(model) - 1):
                 print('Calculating new layer thicknesses for given sub-layers')
-                subl_factor = int((len(model) - 1) / len(self.Mod['Hl']) - 1)
+                subl_factor = int((len(model) - 1) / (len(self.Mod['Hl']) - 1))
                 print(subl_factor)  # how many sub-layers were used
                 # # hl = np.zeros(len(model)-1)
                 hl = []