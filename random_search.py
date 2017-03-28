ff7dcdc1c0f746d475c991338bdc585759d442c0
james <jamesholt92@hotmail.com>
2017-03-28 17:45:37 +0100

    sim1D: renamed site_model_reset to reset_site_model (more appropriate) analysis_funcs: in process of fixing for sub-layers

--- random_search.py
+++ random_search.py
@@ -16,10 +16,10 @@ __author__ = 'James Holt'
 
 def main():
     site = 'FKSH11'
-    iterations = 10
+    iterations = 5000
     name = site+'_rnd_ufm_'+'_'+str(iterations)+'_run_0'
-    wd = '/data/share/Japan/SiteInfo/S_B/FKSH11_synth_WN/'
-    rd = '/data/share/Japan/SiteInfo/S_B/{0}_synth_Vs_MC_subl/'.format(site)
+    wd = '/data/share/Japan/SiteInfo/S_B/Repi_lessthan_300_depth_lessthan_25/'
+    rd = '/data/share/Japan/SiteInfo/S_B/{0}_Vs_MC_subl/'.format(site)
 
     if not os.path.isdir(rd):  # if not a directory, make it
         print('creating dir: {0}'.format(rd))
@@ -32,7 +32,7 @@ def main():
             name = name.replace('run_0', 'run_{0}'.format(j))
         print('name changed to: {0}'.format(name))
 
-    sub_uniform_search(site, wd, rd, 25, 10, iterations, name, (0.4, 0.6), 1, 0, (0.5, 25), 100, False, False, None,
+    sub_uniform_search(site, wd, rd, 250.0, 10, iterations, name, (0.4, 0.6), 1, 0, (0.5, 25), 100, False, False, None,
                        (1, 2, 3))
 
 
