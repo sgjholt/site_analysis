133910d704fa7b59f42c8693468ee1e12f47fe93
james <jamesholt92@hotmail.com>
2017-05-08 12:36:09 +0100

    updating sim1D to include konno_ohmachi smoothing of forward model - in progress

--- random_search.py
+++ random_search.py
@@ -19,7 +19,8 @@ def main():
     site = sys.argv[1]
     iterations = int(sys.argv[2])
     motion = str(sys.argv[3])
-    name = site+'_rnd_ufm_'+'_'+str(iterations)+'_run_0'
+    konno_ohmachi = 20
+    name = site + '_rnd_ufm_' + '_' + str(iterations) + '_run_0_'
     wd = '/data/share/Japan/SiteInfo/S_B/Repi_lessthan_300_depth_lessthan_25/'
     rd = '/data/share/Japan/SiteInfo/S_B/{0}_Vs_MC_subl_{1}/'.format(site, motion)
 
@@ -34,18 +35,18 @@ def main():
             name = name.replace('run_0', 'run_{0}'.format(j))
         print('name changed to: {0}'.format(name))
 
-    sub_uniform_search(site, wd, rd, 250.0, 10, iterations, name, 0, (0.5, 25), 100, False, False, None,
-                       (1, 2, 3), motion)
+    sub_uniform_search(site, wd, rd, 25, 10, iterations, name, 0, (0.5, 25), 100, False, False, None,
+                       (1, 2, 3), motion, konno_ohmachi)
 
 
 def sub_uniform_search(*args):
     site, wd, rd, pct, steps, iters, name, i_ang, x_range, const_q, elastic, cadet_correct, \
-    fill_troughs, n_sub_layers, motion = args
+    fill_troughs, n_sub_layers, motion, konno_ohmachi = args
     titles = ['site', 'SB_dir', 'run_dir', 'pct_variation', 'model_steps', 'iterations',
               'i_ang', 'x_range_for_xcor', 'const_q', 'elastic', 'cadet_correct', 'fill_troughs', 'n_sub_layers',
-              'motion']
+              'motion', 'konno_ohmachi_smoothing(b)']
     vrs = [site, wd, rd, pct, steps, iters, i_ang, x_range, const_q, elastic, cadet_correct, fill_troughs,
-           n_sub_layers, motion]
+           n_sub_layers, motion, konno_ohmachi]
 
     with open(rd+name+'.cfg', 'wt') as f:
         f.write('config file for {0}'.format(name).upper())
@@ -60,7 +61,7 @@ def sub_uniform_search(*args):
     site.uniform_sub_random_search(pct_variation=pct, steps=steps, iterations=iters, name=name,
                                    i_ang=i_ang, x_cor_range=x_range, const_q=const_q, elastic=elastic,
                                    cadet_correct=cadet_correct, fill_troughs_pct=fill_troughs, save=True,
-                                   n_sub_layers=n_sub_layers, motion=motion)
+                                   n_sub_layers=n_sub_layers, motion=motion, konno_ohmachi=konno_ohmachi)
 
 
 #def uniform_search(*args):
