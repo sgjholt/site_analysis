b3ec7cbac34e08966f93a8e56ec53c331dcabfa8
james <jamesholt92@hotmail.com>
2017-05-09 22:21:30 +0100

    Added function to inspect misfit space in analysis funcs - removed unused params in sim1D

--- sim1D.py
+++ sim1D.py
@@ -434,7 +434,7 @@ class Sim1D(sc.Site, sm.Site1D):
         self.__add_site_profile()
         self.vp_vs = np.array(self.Mod['Vp']) / np.array(self.Mod['Vs'])
 
-    def read_post_sim_data(self, directory, run=0):
+    def read_post_sim_data(self, directory, run=0, its=5000):
         directory = '/data/share/Japan/SiteInfo/S_B/' + directory
-        self.sim_pars = parse_simulation_cfg(glob.glob(directory+'*run_{0}*.cfg'.format(run))[0])
-        return [pd.read_csv(df, index_col=0) for df in sorted(glob.glob(directory + '*run_{0}*.csv'.format(run)))]
+        self.sim_pars = parse_simulation_cfg(glob.glob(directory+'*{1}_run_{0}*.cfg'.format(run, its))[0])
+        return [pd.read_csv(df, index_col=0) for df in sorted(glob.glob(directory + '*{1}_run_{0}*.csv'.format(run, its)))]
