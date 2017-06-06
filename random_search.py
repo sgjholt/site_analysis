135cc39f5e5b0e2d297fb3061217327c8b78f7ee
james <jamesholt92@hotmail.com>
2017-06-06 11:01:00 +0100

    When log-sampling of input waveform is chosen, x-correlation is now fixed. Returns frequency lag in log units. Added new return of misfit function - power of correlation is also returned for 3D misfit analysis.

--- random_search.py
+++ random_search.py
@@ -21,8 +21,13 @@ def main():
     
     :return: 
     """
-    site = sys.argv[1]
-    iterations = int(sys.argv[2])
+    try:
+        site = sys.argv[1]
+        iterations = int(sys.argv[2])
+    except IndexError:
+        print('Minimum input arguments not given: need site name and # of iters.')
+        print('Exiting: no tasks performed.')
+        sys.exit()
 
     try:
         motion = str(sys.argv[3])
@@ -32,8 +37,10 @@ def main():
         konno_ohmachi = None
 
     name = site + '_rnd_ufm_' + '_' + str(iterations) + '_run_0_'
-    wd = '/data/share/Japan/SiteInfo/S_B/Repi_lessthan_300_depth_lessthan_25/'
-    rd = '/data/share/Japan/SiteInfo/S_B/{0}_Vs_MC_subl_{1}_smooth-{2}/'.format(site, motion, konno_ohmachi)
+    wd = '/data/share/Japan/SiteInfo/S_B/VLER10_blind/'
+    # rd = '/data/share/Japan/SiteInfo/S_B/{0}_Vs_MC_subl_{1}_smooth-{2}/'.format(site, motion, konno_ohmachi)
+    rd = '/data/share/Japan/SiteInfo/S_B/VLER10_blind/'
+    # TODO: Revert changes after Valerio blind test
 
     if not os.path.isdir(rd):  # if not a directory, make it
         print('creating dir: {0}'.format(rd))
@@ -46,8 +53,8 @@ def main():
             name = name.replace('run_0', 'run_{0}'.format(j))
         print('name changed to: {0}'.format(name))
 
-    sub_uniform_search(site, wd, rd, 25, 10, iterations, name, 0, (0.5, 25), 100, False, False, None,
-                       (1, 2, 3, 4), motion, konno_ohmachi)
+    sub_uniform_search(site, wd, rd, 50, 20, iterations, name, 0, (0, 25), None, False, False, None,
+                       (10,), motion, konno_ohmachi)
 
 
 def sub_uniform_search(*args):
@@ -56,6 +63,10 @@ def sub_uniform_search(*args):
     titles = ['site', 'SB_dir', 'run_dir', 'pct_variation', 'model_steps', 'iterations',
               'i_ang', 'x_range_for_xcor', 'const_q', 'elastic', 'cadet_correct', 'fill_troughs', 'n_sub_layers',
               'motion', 'konno_ohmachi_smoothing(b)']
+
+    if const_q is None:
+        const_q = 'Valerio-ETH-model'
+
     vrs = [site, wd, rd, pct, steps, iters, i_ang, x_range, const_q, elastic, cadet_correct, fill_troughs,
            n_sub_layers, motion, konno_ohmachi]
 
