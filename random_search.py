dee4d98de503b2d74fc4ade55e57df201aec2801
james <jamesholt92@hotmail.com>
2017-06-14 18:23:35 +0100

    In progress of adding new method to optimise profile.

--- random_search.py
+++ random_search.py
@@ -120,7 +120,7 @@ def rect_space_search(*args):
         const_q = None
     site = sd.Sim1D(site, working_directory=wd, run_dir=rd)
 
-    site.rect_space_search(*args[3:])
+    site.rect_space_search(args[3], args[4], args[5], args[6], args[7], save=True, log_sample=args[-1])
 
 if __name__ == '__main__':
     start_time = time.time()
