791a05c97dd83a1cb10d0fe9200dcfd15ca14414
James Holt <j.holt@liverpool.ac.uk>
2017-06-15 10:42:15 +0100

    Updated README file - removed blank line from utils

--- random_search.py
+++ random_search.py
@@ -120,8 +120,7 @@ def rect_space_search(*args):
         const_q = None
     site = sd.Sim1D(site, working_directory=wd, run_dir=rd)
 
-    site.rect_space_search(low=70, high=3500, steps=20, iterations=10, name=name, save=True,
-                           log_sample=(250, np.exp(1)))
+    site.rect_space_search(low=70, high=3500, steps=20, iterations=10, elastic=False, name=name, save=True)
 
 if __name__ == '__main__':
     start_time = time.time()
