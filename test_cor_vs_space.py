d96b4388866fd8c759b733ff89ee51f38d5d5f00
james <jamesholt92@hotmail.com>
2017-06-20 15:36:07 +0100

    Committed Kamai Vs-Z profile for JP and CA - see Kamai et al. 2016 (Earthquake Spectra)

--- test_cor_vs_space.py
+++ test_cor_vs_space.py
@@ -2,17 +2,19 @@ import numpy as np
 import scipy.stats as stats
 import matplotlib.pyplot as plt
 import random
+import seaborn as sb
 
 v_mod = np.array([100, 200, 600, 300, 900, 1700, 1700])
 scale = 1  # ln units for std
 cor_co = 0.5
-dists = []
 count = 1000
-for vel in v_mod:
+dists = np.zeros((v_mod.size, count))
+
+for i, vel in enumerate(v_mod):
     a, b = np.max(np.array([(np.log(30) - np.log(vel)) / scale, -2])), np.min(
         np.array([(np.log(5000) - np.log(vel)) / scale, 2]))
     pdf = stats.truncnorm(a, b, np.log(vel), scale=scale)
-    dists.append((pdf.rvs(1000) - np.log(vel)) / scale)
+    dists[i] = (pdf.rvs(count) - np.log(vel)) / scale
 
 # fig, ax = plt.subplots(2, 3)
 
@@ -23,10 +25,15 @@ for vel in v_mod:
 
 
 for i, c_layer in enumerate(dists[1:]):
-    if i == 0:
-        dists[i + 1] = (cor_co * dists[0]) + c_layer * (np.sqrt(1 - cor_co ** 2))
-    else:
-        dists[i + 1] = (cor_co * dists[i]) + c_layer * (np.sqrt(1 - cor_co ** 2))
+    all_replaced = False
+    tmp = (cor_co * dists[i]) + c_layer * (np.sqrt(1 - cor_co ** 2))
+    while not all_replaced:
+        locs = np.where((tmp <= np.log(75)) & (tmp <= np.log(5000)))
+        replace = (pdf.rvs(len(locs[0])) - np.log(v_mod[i + 1])) / scale
+        tmp[locs] = (cor_co * dists[i][locs]) + c_layer[locs] * (np.sqrt(1 - cor_co ** 2))
+    dists[i + 1] = tmp
+    #  dists[i + 1] = (cor_co * dists[i]) + c_layer * (np.sqrt(1 - cor_co ** 2))
+
 
 layers = [x + 0.5 for x in range(len(v_mod))]
 plt.figure()
@@ -73,3 +80,24 @@ plt.ylabel('Layer')
 plt.ylim([0.5, 6.5])
 plt.gca().invert_yaxis()
 
+# ---------------KAMAI | VS-Z | (2016) TESTING-------------------------------#
+sb.set_style('white')
+sb.set_context('talk')
+
+Vs30s = [200, 300, 500, 700]
+depths = np.linspace(0, 100, 101)
+
+fig, axs = plt.subplots(1, len(Vs30s), sharey=True, sharex=True)
+axs.flatten()[0].invert_yaxis()
+for i, out in enumerate(zip(axs.flatten(), Vs30s)):
+    ax, Vs30 = out
+    ax.plot(kamai_vsz(depths, Vs30)[0], depths, 'k')
+    ax.plot(kamai_vsz(depths, Vs30)[1][0], depths, 'r--')
+    ax.plot(kamai_vsz(depths, Vs30)[1][1], depths, 'r--')
+    if i == 0:
+        ax.set_ylabel('$Depth$ $[m]$')
+    ax.set_xlabel('$V_s$ $[m/s]$')
+    ax.set_title('$V_s$$30$: ${0}$ $[m/s]$'.format(Vs30))
+    ax.grid(which='both')
+
+fig.suptitle('Kamai et al., 2016 | Vs-Z | Japan')
